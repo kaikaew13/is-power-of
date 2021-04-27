0b9ac9a6282774a911e818c1f9388ecdde0baa94
kaikaew13 <ksaratunti@gmail.com>
2021-04-27 22:03:52 +0700

    setting up

---
+++ index.js
@@ -0,0 +1,20 @@
+const MIN = 1;
+const MAX = 10;
+const ARR = [
+  1,
+  1073741824,
+  1162261467,
+  1073741824,
+  1220703125,
+  362797056,
+  1977326743,
+  1073741824,
+  387420489,
+  1000000000,
+];
+
+module.exports = (n, baseNum) => {
+  if (baseNum > MAX || baseNum < MIN)
+    throw new Error('base number is not in the supported range');
+  return n > 0 && ARR[baseNum - 1] % n == 0;
+};
